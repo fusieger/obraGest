87bca503c05d38e74d5c1b93aa8517cbc0275d6e
Guilherme Fusieger <62902494+fusieger@users.noreply.github.com>
2025-07-06 11:18:09 -0300

    Changes to be committed:  	modified:   cronogramas.html 	modified:   financeiro.html 	modified:   js/logout.js 	modified:   materiais.html 	modified:   obras.html
    Adicionado o cabeçalho nas demais páginas, para permitir a navegação entre elas através do dashboard.

--- js/logout.js
+++ js/logout.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-// Exibir e-mail do usuário
+//exibir e-mail do usuário
 const email = sessionStorage.getItem('userEmail');
 document.getElementById('username').textContent = email;
 
-// Logout
-document.getElementById('logoutBtn').addEventListener('click', () => {
-    sessionStorage.removeItem('userEmail');
-    window.location.href = 'index.html';
+//logout
+const logoutBtn = document.getElementById('logoutBtn');
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => {
+      sessionStorage.removeItem('userEmail');
+      window.location.href = 'index.html';
     });
+  }
 });
